3940124230354036570372c3481c9a4e5c3575ff
Francis Chan <francis@166.111.69.204>
2013-09-16 11:52:34 +0800

    benchmarks on Bio

--- BioResponse/Benchmarks/rf_benchmark.py
+++ BioResponse/Benchmarks/rf_benchmark.py
@@ -1,21 +1,39 @@
 #!/usr/bin/env python
 
 from sklearn.ensemble import RandomForestClassifier
-import csv_io
+from sklearn import cross_validation
+import evalfun
 import math
+import numpy as np
 
 def main():
-    train = csv_io.read_data("../Data/train.csv")
-    target = [x[0] for x in train]
-    train = [x[1:] for x in train]
-    test = csv_io.read_data("../Data/test.csv")
+#    train = csv_io.read_data("../Data/train.csv")
+    dataset = np.genfromtxt(open('../Data/train.csv','r'), delimiter=',', dtype='f8')[1:]
+    target = np.array([x[0] for x in dataset])
+    train = np.array([x[1:] for x in dataset])
+#    test = csv_io.read_data("../Data/test.csv")
 
-    rf = RandomForestClassifier(n_estimators=100, min_split=2)
-    rf.fit(train, target)
-    predicted_probs = rf.predict_proba(test)
-    predicted_probs = ["%f" % x[1] for x in predicted_probs]
-    csv_io.write_delimited_file("../Submissions/rf_benchmark.csv",
-                                predicted_probs)
+    rf = RandomForestClassifier(n_estimators=100)
+
+    #Simple K-Fold cross validation. 5 folds.
+    cv = cross_validation.KFold(len(train), k=5, indices=False)
+
+    #iterate through the training and test cross validation segments and
+    #run the classifier on each one, aggregating the results into a list
+    results = []
+    for traincv, testcv in cv:
+        rf.fit(train[traincv], target[traincv])
+        probas = rf.predict_proba(train[testcv])
+        results.append(evalfun.llfun(target[testcv], [x[1] for x in probas]) )
+
+    #print out the mean of the cross-validated results
+    print "Results: " + str(np.array(results).mean() )
+
+#    rf.fit(train, target)
+#    predicted_probs = rf.predict_proba(test)
+#    predicted_probs = ["%f" % x[1] for x in predicted_probs]
+#    csv_io.write_delimited_file("../Submissions/rf_benchmark.csv",
+#                                predicted_probs)
 
 if __name__=="__main__":
     main()
--- BioResponse/Benchmarks/svm_benchmark.py
+++ BioResponse/Benchmarks/svm_benchmark.py
@@ -1,21 +1,39 @@
 #!/usr/bin/env python
 
 from sklearn import svm
-import csv_io
-import math
+from sklearn import cross_validation
+import evalfun
+import numpy as np
 
 def main():
-    train = csv_io.read_data("../Data/train.csv")
-    target = [x[0] for x in train]
-    train = [x[1:] for x in train]
-    test = csv_io.read_data("../Data/test.csv")
+#    train = csv_io.read_data("../Data/train.csv")
+#    target = [x[0] for x in train]
+#    train = [x[1:] for x in train]
+#    test = csv_io.read_data("../Data/test.csv")
 
-    svc = svm.SVC(probability=True)
-    svc.fit(train, target)
-    predicted_probs = svc.predict_proba(test)
-    predicted_probs = ["%f" % x[1] for x in predicted_probs]
-    csv_io.write_delimited_file("../Submissions/svm_benchmark.csv",
-                                predicted_probs)
+    dataset = np.genfromtxt(open('../Data/train.csv','r'), delimiter=',', dtype='f8')[1:]
+    target = np.array([x[0] for x in dataset])
+    train = np.array([x[1:] for x in dataset])
+
+    cfr = svm.SVC(probability=True)
+
+    #Simple K-Fold cross validation. 5 folds.
+    cv = cross_validation.KFold(len(train), k=5, indices=False)
+
+    results = []
+    for traincv, testcv in cv:
+        cfr.fit(train[traincv], target[traincv])
+        probas = cfr.predict_proba(train[testcv])
+        results.append(evalfun.llfun(target[testcv], [x[1] for x in probas]) )
+
+    #print out the mean of the cross-validated results
+    print "Results: " + str(np.array(results).mean() )
+
+#    svc.fit(train, target)
+#    predicted_probs = svc.predict_proba(test)
+#    predicted_probs = ["%f" % x[1] for x in predicted_probs]
+#    csv_io.write_delimited_file("../Submissions/svm_benchmark.csv",
+#                                predicted_probs)
 
 if __name__=="__main__":
     main()
